62aa7825996ec19e9d264c6f1e50c0bdf2a674c4
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-11 09:22:55 -0400

    code fix: player1 & player2 are instance variables

--- game.rb
+++ game.rb
@@ -10,14 +10,14 @@ class Game
   end
 
   def game_loop
-    while (player1.game_lives > 0 && player2.game_lives > 0)
+    while (@player1.game_lives > 0 && @player2.game_lives > 0)
       # check whose turn is it
-      player = @questionNumber % 2 == 0 ? player1 : player2
+      player = @questionNumber % 2 == 0 ? @player1 : @player2
 
       # ask a question to player
       question = Question.new
       print "Player"
-      print player == player1 ? " 1: " : " 2: "
+      print player == @player1 ? " 1: " : " 2: "
       puts question.pose
 
       # get user's answer and check if it is correct
