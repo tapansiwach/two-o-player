0981f9db9c0ae6fa93e4e03dde9fc98dfc5ef11d
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-11 09:14:08 -0400

    ask a question to player in the game loop

--- game.rb
+++ game.rb
@@ -11,7 +11,14 @@ class Game
 
   def game_loop
     while (player1.game_lives > 0 && player2.game_lives > 0)
+      # check whose turn is it
+      player = @questionNumber % 2 == 0 ? player1 : player2
 
+      # ask a question to player
+      question = Question.new
+      print "Player"
+      print player == player1 ? " 1: " : " 2: "
+      puts question.pose
     end
     puts "Good bye!"
   end
