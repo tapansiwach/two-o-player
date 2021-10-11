aeb273a2c512734e0385d4b72d424ac681768b43
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-11 09:13:17 -0400

    setup a game loop

--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
 require './player'
+require './question'
 
 class Game
 
@@ -6,6 +7,13 @@ class Game
     @player1 = Player.new
     @player2 = Player.new
     @questionNumber = 0
+  end
+
+  def game_loop
+    while (player1.game_lives > 0 && player2.game_lives > 0)
+
+    end
+    puts "Good bye!"
   end
 
 end
